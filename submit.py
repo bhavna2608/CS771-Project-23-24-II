c1b9c8d83be87c8c8d676ab4b8c34c99bb9821e6
Bhavna Jayswal <139497517+bhavna2608@users.noreply.github.com>
2024-08-25 14:32:03 +0530

    Update submit.py

--- submit.py
+++ submit.py
@@ -1,11 +1,7 @@
 import numpy as np
 import sklearn
-from scipy.linalg import khatri_rao
 from sklearn.svm import LinearSVC
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import RidgeClassifier
 
 # You are allowed to import any submodules of sklearn that learn linear models e.g. sklearn.svm etc
 # You are not allowed to use other libraries such as keras, tensorflow etc
@@ -20,50 +16,37 @@ from sklearn.linear_model import RidgeClassifier
 # You may define any new functions, variables, classes here
 # For example, functions to calculate next coordinate or step length
 
-clf = LogisticRegression(C = 10, tol=0.1, solver='lbfgs', penalty='l2', max_iter=300)
-
-
 ################################
 # Non Editable Region Starting #
 ################################
 def my_fit( X_train, y_train ):
-################################
-#  Non Editable Region Ending  #
-################################
+    x_train=my_map(X_train)
+    # model = LinearSVC(C=0.15,max_iter=110)
+    model = LogisticRegression(C=10, solver='lbfgs',penalty='l2', max_iter=300,dual="auto", tol=0.1)
+    model.fit(x_train, y_train)
+    w = model.coef_
+    b = model.intercept_
+    w=w.T[:,-1]
 
-	# Use this method to train your model using training CRPs
-	# X_train has 32 columns containing the challeenge bits
-	# y_train contains the responses
-	
-	# THE RETURNED MODEL SHOULD BE A SINGLE VECTOR AND A BIAS TERM
-	# If you do not wish to use a bias term, set it to 0
-    X_train = my_map(X_train)
-    clf.fit(X_train, y_train)
-    w = clf.coef_[0]  # Access the coefficients via the named_steps attribute
-    b = clf.intercept_[0]
-    
     return w, b
 
-
-################################
-# Non Editable Region Starting #
-################################
 def my_map( X ):
-################################
-#  Non Editable Region Ending  #
-################################
+  feat=[]
+  X_new = np.array([X])
+ 
+  mapp_X = 1-2*X_new
+ 
+  mapp_X= np.squeeze(mapp_X)
 
-	# Use this method to create features.
-	# It is likely that my_fit will internally call my_map to create features for train points
-    d = X.shape[0]
-    u = np.ones((d,1))
-    X = np.concatenate((X, u), axis=1)
-    n = X.shape[1]
-    n = np.shape(X)[1]
-    X = np.cumprod(np.flip(2 * X - 1, axis=1), axis=1)
-    feat = khatri_rao(X.T, X.T).T
-    upp_mat = np.triu(np.ones((n, n)), k=0)-np.eye(n)
-    u1d = upp_mat.flatten().astype(bool)
-    feat = feat[:,u1d]
-    
-    return feat
+  flp=np.flip( mapp_X , axis = 1 )
+  X_raw = np.cumprod( flp, axis = 1 )
+  B=[]
+  for i in range(32):
+    for j in range(i,32):
+      product = X_raw[:,i] * X_raw[:,j]
+      if i==j:
+        product=X_raw[:,i]
+      B.append(product)
+  feat.append(np.array(B))
+  feat=np.concatenate(feat, axis=0)
+  return ((feat.T))
